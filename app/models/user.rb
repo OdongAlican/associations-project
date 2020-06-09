a570c6dfe43f28ea3e9e14637afa8fd0112a2e2f
OdongAlican <sandieo.2020@gmail.com>
2020-06-10 00:30:32 +0300

    adds association model between event and user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
     has_many :created_events, foreign_key: "creator_id", class_name: "Event"
+    has_many :event_attendings, foreign_key: :attendee_id, dependent: :destroy
+    has_many :attended_events, through: :event_attendings
     has_secure_password
 end
