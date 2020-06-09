81ad4adad84b9d28c21f78621d80be890a7c0e7a
OdongAlican <sandieo.2020@gmail.com>
2020-06-09 13:35:38 +0300

    Adds the following - Adds model for Events - Adds a migration for creator_id in events table - creates association between User and Event

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_08_123629) do
+ActiveRecord::Schema.define(version: 2020_06_09_101101) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "location"
+    t.date "event_date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "creator_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
